dec8641177d166a7144c74f67986a658fcbc8c7a
Юлия Медведева <medvedevajulev@gmail.com>
2024-11-03 19:31:08 +0300

    added solutions for oop and operations system

--- operationsystem/labwork4/handle_sigusr1.c
+++ operationsystem/labwork4/handle_sigusr1.c
@@ -1,25 +1,25 @@
 #include <stdio.h>
 #include <signal.h>
-#include <unistd.h>
+#include <windows.h>
 
 // Обработчик сигнала
-void handle_sigusr1(int sig) {
-    printf("Получен сигнал SIGUSR1!\n");
+static void handle_sigusr1(int sig) {
+	printf("Получен сигнал SIGUSR1!\n");
 }
 
 int main() {
-    // Регистрация обработчика для SIGUSR1
-    if (signal(SIGUSR1, handle_sigusr1) == SIG_ERR) {
-        perror("Ошибка регистрации сигнала");
-        return 1;
-    }
+	// Регистрация обработчика для SIGUSR1
+	if (signal(SIGUSR1, handle_sigusr1) == SIG_ERR) {
+		perror("Ошибка регистрации сигнала");
+		return 1;
+	}
 
-    printf("Ожидание сигнала SIGUSR1... Мой PID: %d\n", getpid());
+	printf("Ожидание сигнала SIGUSR1... Мой PID: %d\n", GetCurrentProcessId());
 
-    // Бесконечный цикл для ожидания сигнала
-    while (1) {
-        pause(); // Ожидание любого сигнала
-    }
+	// Бесконечный цикл для ожидания сигнала
+	while (1) {
+		Sleep(INFINITE); // Ожидание любого сигнала
+	}
 
-    return 0;
+	return 0;
 }
