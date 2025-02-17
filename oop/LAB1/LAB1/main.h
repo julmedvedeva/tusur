2bd7941fad64027729428db7ee89fa470f97b97e
Юлия Медведева <60058448+julmedvedeva@users.noreply.github.com>
2025-02-17 16:11:47 +0300

    oop lw1: fixed after review (#4)
    * добавлены проверки вводимых данных
    * fixed settsetted value for cyrillic mark name
    * clean up

--- oop/LAB1/LAB1/main.h
+++ oop/LAB1/LAB1/main.h
@@ -6,42 +6,35 @@
 
 class Car {
 private:
-    char* brand_;
+    std::wstring brand_; // Используем std::wstring
     int number_;
     float price_;
 
 public:
     Car(); // Конструктор по умолчанию
-    Car(const char* brand, int number, float price); // Конструктор с параметрами
+    Car(const wchar_t* brand, int number, float price); // Конструктор с параметрами
     virtual ~Car(); // Деструктор
 
     virtual void Print() const; // Метод вывода информации
     void Input(); // Метод ввода информации
 
     // Методы доступа (геттеры и сеттеры)
-    const char* GetBrand() const { return brand_; }
+    const wchar_t* GetBrand() const { return brand_.c_str(); }
     int GetNumber() const { return number_; }
     float GetPrice() const { return price_; }
-    void SetBrand(const char* brand);
+    void SetBrand(const wchar_t* brand);
     void SetNumber(int number);
     void SetPrice(float price);
 };
 
-class ACar : public Car {
+class AdditionalCar : public Car {
 private:
-    std::string mainInfo_;
+    const wchar_t* mainInfo_;
 
 public:
-    ACar(); // Конструктор по умолчанию
-    ACar(const char* brand, const std::string& mainInfo, int number, float price);
-    virtual ~ACar(); // Деструктор
+    AdditionalCar(); // Конструктор по умолчанию
+    AdditionalCar(const wchar_t* brand, const wchar_t* mainInfo, int number, float price);
+    virtual ~AdditionalCar(); // Деструктор
     void Print() const override; // Переопределяем метод Print
 };
 
-class CCar : public Car {
-public:
-    CCar(); // Конструктор по умолчанию
-    CCar(const char* brand, int number, float price);
-    virtual ~CCar(); // Деструктор
-    void Print() const override; // Переопределяем метод Print
-};
